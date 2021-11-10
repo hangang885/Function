b957da06a5b89c2c88acde40c87ae833e7aeadb6
hangang885 <hangang885@gmail.com>
2021-11-10 09:09:21 +0900

    권한을 확인하지 않아, 전화가 안되는 부분 수정

--- app/src/main/java/com/example/myapplication/CallActivity.kt
+++ app/src/main/java/com/example/myapplication/CallActivity.kt
@@ -1,13 +1,17 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.PhoneNumberFormattingTextWatcher
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myapplication.databinding.ActivityCallBinding
 import com.example.myapplication.databinding.ActivityMapBinding
+import java.util.jar.Manifest
 
 class CallActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityCallBinding
@@ -15,7 +19,7 @@ class CallActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-         mBinding = ActivityCallBinding.inflate(layoutInflater)
+        mBinding = ActivityCallBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
 
@@ -30,18 +34,57 @@ class CallActivity : AppCompatActivity() {
         mBinding.writeNumber.addTextChangedListener(PhoneNumberFormattingTextWatcher())
     }
 
-    fun onClick(view:View){
+    fun onClick(view: View) {
 
-        when(view){
-            mBinding.opencall ->{
-                var intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: ${mBinding.writeNumber.text}"))
+        when (view) {
+            mBinding.opencall -> {
+                var intent =
+                    Intent(Intent.ACTION_DIAL, Uri.parse("tel: ${mBinding.writeNumber.text}"))
                 startActivity(intent)
             }
-            mBinding.call ->{
-                var intent = Intent(Intent.ACTION_CALL, Uri.parse("tel: ${mBinding.writeNumber.text}"))
-                startActivity(intent)
+            mBinding.call -> {
+
+                if(CheckPermession()){
+                    var intent =
+                        Intent(Intent.ACTION_CALL, Uri.parse("tel: ${mBinding.writeNumber.text}"))
+                    startActivity(intent)
+                }
             }
 
         }
     }
+
+    fun CheckPermession(): Boolean {
+        if ((ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CALL_PHONE
+            )) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.CALL_PHONE
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.CALL_PHONE
+                    ), 100
+                )
+                true
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.CALL_PHONE
+                    ), 100
+                )
+                false
+            }
+        }
+
+        return true
+    }
+
 }
